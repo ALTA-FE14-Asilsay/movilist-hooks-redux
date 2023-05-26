9ea45f06b0fe2520469e28301ee382b2cdb80a26
Asilsay <lamufaku@gmail.com>
2023-05-26 19:37:56 +0700

    feat: migrate favorite, error  cc to fc

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,41 +1,84 @@
-import { Component } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Layout, Section } from '../components/Layout';
-import { data } from '../json/dummy.json';
 import { Card } from '../components/Card';
-import { withRouter } from '../withRouter';
+import Spinner from '../components/Loading';
+import { GetMovieType } from '../utils/movieType';
+import api from '../utils/api';
+import { useNavigate } from 'react-router-dom';
+import withReactContent from 'sweetalert2-react-content';
+import swal from '../utils/swal';
 
-export class Favorites extends Component {
-  render() {
-    return (
-      <Layout>
-        <Section
-          addClass="bg-base-100 px-16 lg:px-24 py-6 pt-16 "
-          id="now-playing-section"
-        >
-          <div className="w-full min-h-screen flex flex-col items-center">
-            <p className="text-3xl mb-6 tracking-wider uppercase font-semibold ">
-              My Favourite Movie
-            </p>
+interface DatasState {
+  isLoading: boolean;
+  datasFavo: Array<GetMovieType>;
+}
 
-            <div className="w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5 ">
-              {data.map((item, index) => {
+export const Favorites: FC<DatasState> = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [datasFavo, setDatasFavo] = useState([]);
+
+  const MySwal = withReactContent(swal);
+
+  const navigate = useNavigate();
+
+  const fetchFavo = async (code: string) => {
+    setIsLoading(true);
+    await api
+      .getFavo(code)
+      .then((response) => {
+        const { data } = response.data.results;
+        setDatasFavo(data);
+        console.log(response);
+      })
+      .catch((error) => {
+        MySwal.fire({
+          icon: 'error',
+          title: 'Failed',
+          text: `error :  ${error.message}`,
+          showCancelButton: false,
+        });
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    fetchFavo('19622220');
+  }, []);
+
+  return (
+    <Layout>
+      <Section
+        addClass="bg-base-100 px-16 lg:px-24 py-16 pt-16 "
+        id="now-playing-section"
+      >
+        <div className="w-full min-h-screen flex flex-col items-center gap-5">
+          <p className="text-3xl mb-6 tracking-wider uppercase font-semibold ">
+            My Favorites
+          </p>
+          {isLoading ? (
+            <div className="w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-5">
+              {datasFavo.map((item: GetMovieType) => {
                 return (
                   <Card
-                    key={index}
+                    key={item.id}
                     id={`movie-${item.title}`}
                     title={item.title}
-                    description={item.description}
-                    image={item.image}
+                    description={item.overview}
+                    image={item.poster_path}
+                    onClick={() => navigate(`${item.id}`)}
                   />
                 );
               })}
             </div>
-            <button className="btn btn-primary btn-wide">Show More</button>
-          </div>
-        </Section>
-      </Layout>
-    );
-  }
-}
+          ) : (
+            <div className="w-full bg-slate-400 h-[450px] flex items-center justify-center">
+              <Spinner />
+            </div>
+          )}
+        </div>
+      </Section>
+    </Layout>
+  );
+};
 
-export default withRouter(Favorites);
+export default Favorites;
